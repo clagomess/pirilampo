a4aa684c87736347bb8996ec7540b1b880674b42
clagomess <cla.gomess@gmail.com>
2016-08-10 15:19:34 -0300

    Parse Document

--- src/br/com/pirilampo/Main.java
+++ src/br/com/pirilampo/Main.java
@@ -2,7 +2,6 @@ package br.com.pirilampo;
 
 import gherkin.*;
 import gherkin.ast.GherkinDocument;
-import gherkin.deps.com.google.gson.Gson;
 
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -11,24 +10,19 @@ import java.io.Reader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Gson gson = new Gson();
         Parser<GherkinDocument> parser = new Parser<>(new AstBuilder());
         TokenMatcher matcher = new TokenMatcher();
 
-        long startTime = System.currentTimeMillis();
-
-        Reader in = new InputStreamReader(new FileInputStream("C:\\_DESENV\\gherkin-java\\EPE001.feature"), "UTF-8");
+        Reader in = new InputStreamReader(new FileInputStream("C:\\Users\\02757713183\\Desktop\\DEMANDAS\\FNDE-1785 SICE\\EPE\\Feature\\FNDE_EPE002_Emitir relatório de dados do Cursista\\EPE001_Dados_Dos_Cursistas.feature"), "UTF-8");
         try {
             GherkinDocument gherkinDocument = parser.parse(in, matcher);
-            Stdio.out.println(gson.toJson(gherkinDocument));
-        } catch (ParserException e) {
-            Stdio.err.println(e.getMessage());
-            System.exit(1);
-        }
 
-        long endTime = System.currentTimeMillis();
-        if(System.getenv("GHERKIN_PERF") != null) {
-            Stdio.err.println(endTime - startTime);
+            if(gherkinDocument != null){
+                ParseDocument pd = new ParseDocument(gherkinDocument);
+                System.out.println(pd.getHtml());
+            }
+        } catch (ParserException e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ src/br/com/pirilampo/ParseDocument.java
@@ -0,0 +1,87 @@
+package br.com.pirilampo;
+
+import gherkin.ast.*;
+
+public class ParseDocument {
+    private GherkinDocument gd;
+
+    public ParseDocument(GherkinDocument gd){
+        this.gd = gd;
+    }
+
+    private final String HTML_TITULO = "<h1>%s</h1>\n";
+    private final String HTML_PARAGRAFO = "<p>%s</p>\n";
+    private final String HTML_STEP = "<p><span class=\"keyword\">%s</span> %s</p>\n";
+    private final String HTML_CODE = "<pre>%s</pre>\n";
+
+    private final String HTML_CHILDREN = "<div class=\"panel panel-default\">\n" +
+            "<div class=\"panel-heading\"><strong>%s</strong></div>\n%s\n</div>\n";
+    private final String HTML_CHILDREN_BODY = "<div class=\"panel-body\">%s</div>\n";
+    private final String HTML_CHILDREN_TABLE = "<div class=\"table-responsive\">\n" +
+            "<table class=\"table table-condensed table-bordered table-hover table-striped\">\n" +
+            "<thead>\n<tr>%s</tr>\n</thead>\n" +
+            "<tbody>\n<tr>%s</tr>\n</tbody>\n" +
+            "</table>\n</div>\n";
+    private final String HTML_CHILDREN_TABLE_TR = "<tr>%s</tr>\n";
+    private final String HTML_CHILDREN_TABLE_TH = "<th>%s</th>\n";
+    private final String HTML_CHILDREN_TABLE_TD = "<td>%s</td>\n";
+
+    public String getHtml(){
+        String html = "";
+
+        if(gd != null){
+            html += String.format(HTML_TITULO, gd.getFeature().getName());
+            html += String.format(HTML_PARAGRAFO, gd.getFeature().getDescription());
+
+            for (ScenarioDefinition sd : gd.getFeature().getChildren()){
+                String body  = "";
+
+                if(sd.getDescription() != null){
+                    body += String.format(HTML_PARAGRAFO, sd.getDescription());
+                }
+
+                for (Step step : sd.getSteps()){
+                    body += String.format(HTML_STEP, step.getKeyword(), step.getText());
+
+                    if(step.getArgument() != null){
+                        if(step.getArgument() instanceof DataTable) {
+                            String htmlTrH = "";
+                            String htmlTrD = "";
+                            int i = 0;
+
+                            for (TableRow tr : ((DataTable) step.getArgument()).getRows()) {
+                                String htmlTc = "";
+                                for (TableCell tc : tr.getCells()) {
+                                    if (i == 0) {
+                                        htmlTc += String.format(HTML_CHILDREN_TABLE_TH, tc.getValue());
+                                    } else {
+                                        htmlTc += String.format(HTML_CHILDREN_TABLE_TD, tc.getValue());
+                                    }
+                                }
+
+                                if (i == 0) {
+                                    htmlTrH += String.format(HTML_CHILDREN_TABLE_TR, htmlTc);
+                                } else {
+                                    htmlTrD += String.format(HTML_CHILDREN_TABLE_TR, htmlTc);
+                                }
+
+                                i++;
+                            }
+
+                            body += String.format(HTML_CHILDREN_TABLE, htmlTrH, htmlTrD);
+                        }
+
+                        if(step.getArgument() instanceof DocString) {
+                            body += String.format(HTML_CODE, ((DocString) step.getArgument()).getContent());
+                        }
+                    }
+                }
+
+                body = String.format(HTML_CHILDREN_BODY, body);
+                html += String.format(HTML_CHILDREN, sd.getName(), body);
+            }
+        }
+
+        return html;
+    }
+}
