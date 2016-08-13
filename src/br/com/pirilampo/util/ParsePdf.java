b3d59536c1ce7dd10038b75369d095d7e8705be8
claudio gomes <cla.gomess@gmail.com>
2016-08-13 03:53:01 -0300

    Nº Página PDF

--- src/br/com/pirilampo/util/ParsePdf.java
+++ src/br/com/pirilampo/util/ParsePdf.java
@@ -1,7 +1,12 @@
 package br.com.pirilampo.util;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
 import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfPageEventHelper;
+import com.itextpdf.text.pdf.PdfTemplate;
 import com.itextpdf.text.pdf.PdfWriter;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 
@@ -13,6 +18,7 @@ public class ParsePdf {
         // Apply preferences and build metadata.
         Document document = new Document(layout.equals("R") ? PageSize.A4 : PageSize.A4.rotate());
         PdfWriter pw = PdfWriter.getInstance(document, new FileOutputStream(path));
+        pw.setPageEvent(new Rodape());
 
         // Build PDF document.
         document.open();
@@ -26,5 +32,22 @@ public class ParsePdf {
         );
 
         document.close();
+    }
+
+    private class Rodape extends PdfPageEventHelper {
+        PdfTemplate total;
+
+        public void onOpenDocument(PdfWriter writer, Document document) {
+            total = writer.getDirectContent().createTemplate(30, 16);
+        }
+
+        public void onEndPage(PdfWriter writer, Document document) {
+            ColumnText.showTextAligned(
+                    writer.getDirectContentUnder(),
+                    Element.ALIGN_CENTER,
+                    new Phrase("Página " + String.valueOf(writer.getPageNumber())),
+                    writer.getPageSize().getWidth() - 70, 20, 0
+            );
+        }
     }
 }
