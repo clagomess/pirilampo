782d6cf69981e773130dbc6a7aff30b7e72d855d
claudio gomes <cla.gomess@gmail.com>
2016-12-19 01:55:33 -0200

    console options

--- src/main/java/br/com/pirilampo/main/Main.java
+++ src/main/java/br/com/pirilampo/main/Main.java
@@ -8,10 +8,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import org.apache.commons.cli.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.URL;
 
 public class Main extends Application {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static final String SYS_TITLE = "Pirilampo";
     public static final String SYS_PATH = "br/com/pirilampo/";
     public static final String SYS_ICON = SYS_PATH + "resources/img_01.png";
@@ -46,6 +50,44 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-        launch(args);
+        if(args.length > 0){
+            CommandLine cmd = consoleOptions(args);
+        }else{
+            launch(args);
+        }
+    }
+
+    private static CommandLine consoleOptions(String[] args){
+        Options options = new Options();
+        Option option;
+
+        options.addOption(new Option("feature", true, "Arquivo *.feature"));
+        options.addOption(new Option("feature_master", true, "Arquivo *.feature master"));
+        options.addOption(new Option("feature_path", true, "Diretório contendo arquivos *.feature"));
+        options.addOption(new Option("feature_path_master", true, "Diretório contendo arquivos *.feature master"));
+        options.addOption(new Option("output", true, "Diretório de saída"));
+
+        option = new Option("name", true, "Nome do projeto");
+        option.setRequired(true);
+        options.addOption(option);
+
+        option = new Option("version", true, "Versão");
+        option.setRequired(true);
+        options.addOption(option);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            logger.info(e.getMessage());
+            formatter.printHelp("Pirilampo", options);
+
+            System.exit(1);
+        }
+
+        return cmd;
     }
 }
