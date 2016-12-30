748da738a56b89e1d26b0c72f9a3bc7e461151f4
clagomess <cla.gomess@gmail.com>
2016-12-30 17:17:00 -0200

    inicio

---
+++ src/main/java/br/com/pirilampo/htmlTemplate/build.bat
@@ -0,0 +1,8 @@
+npm install -g bower
+npm install -g gulp-cli
+npm install gulp
+npm install gulp-concat
+npm install gulp-uglify
+npm install gulp-minify-css
+bower install
+gulp
---
+++ src/main/java/br/com/pirilampo/htmlTemplate/gulpfile.js
@@ -0,0 +1,58 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var minify = require('gulp-minify-css');
+
+gulp.task('js', function () {
+    gulp.src([
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/bootstrap/dist/js/bootstrap.js',
+        'bower_components/handlebars/handlebars.js',
+        'bower_components/typeahead.js/dist/typeahead.bundle.js',
+        'bower_components/google-diff-match-patch/javascript/diff_match_patch_uncompressed.js',
+        'bower_components/jQuery.PrettyTextDiff/jquery.pretty-text-diff.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-resource/angular-resource.js',
+        'bower_components/angular-route/angular-route.js',
+        'bower_components/angular-ui-router/release/angular-ui-router.js',
+        'js/app.js',
+        'js/featureController.js',
+        'js/template-feature-pasta.js'
+    ])
+        .pipe(concat('feature-pasta.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('css', function () {
+    gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bower_components/lightbox2/dist/css/lightbox.css',
+        'css/simple-sidebar.css',
+        'css/feature-base.css',
+        'css/template-feature-pasta.css'
+    ])
+        .pipe(concat('feature-pasta.min.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'css/feature-base.css',
+        'css/template-feature.css'
+    ])
+        .pipe(concat('feature.min.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/'));
+
+    gulp.src([
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'css/feature-base.css',
+        'css/template-feature-pdf.css'
+    ])
+        .pipe(concat('feature-pdf.min.css'))
+        .pipe(minify())
+        .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', ['js', 'css'], function () {});
